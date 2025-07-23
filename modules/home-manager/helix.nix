07c83498453e8fdb5c8e9990a89b5c5a9ae3163c
Elmar Schug <elmar.schug@jayware.org>
2025-07-23 09:54:52 +0200

    Adjusted key bindings of helix editor.

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -17,12 +17,25 @@
       defaultEditor = true; # TODO: does it really work?
       settings = {
         theme = "dark_plus";
-        editor.whitespace.render = {
-          space = "all";
-          tab = "all";
-          nbsp = "none";
-          nnbsp = "none";
-          newline = "none";
+        editor = {
+          color-modes = true;
+          statusline = {
+            mode.normal = "NORMAL";
+            mode.insert = "INSERT";
+            mode.select = "SELECT";
+          };
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+          whitespace.render = {
+            space = "all";
+            tab = "all";
+            nbsp = "none";
+            nnbsp = "none";
+            newline = "none";
+          };
         };
         keys.normal = {
           C-right = "move_next_word_start";
@@ -37,6 +50,8 @@
           C-left = "move_prev_word_end";
           C-S-right = "extend_next_word_end";
           C-S-left = "extend_prev_word_start";
+        };
+        keys.select = {
           tab = "indent";
           S-tab = "unindent";
         };
