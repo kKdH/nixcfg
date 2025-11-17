6988ac8b1f3b2d9d764c9710034f60248da7cb13
Elmar Schug <elmar.schug@jayware.org>
2025-11-17 19:12:03 +0100

    Configured rust language server for helix editor.

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -36,6 +36,13 @@
             nnbsp = "none";
             newline = "none";
           };
+          lsp = {
+            display-messages = true;
+          };
+          inline-diagnostics = {
+            cursor-line = "hint";
+            other-lines = "disable";
+          };
         };
         keys.normal = {
           C-right = "move_next_word_start";
@@ -54,6 +61,24 @@
         keys.select = {
           tab = "indent";
           S-tab = "unindent";
+        };
+      };
+      languages = {
+        language = [
+          {
+            name = "rust";
+            auto-format = true;
+            formatter.command = "${pkgs.rustfmt}/bin/rustfmt";
+          }
+        ];
+        language-server = {
+          rust-analyzer = {
+            command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+            config = {
+              check = { command = "${pkgs.clippy}/bin/cargo-clippy"; };
+              cargo = { features = "all"; };
+            };
+          };
         };
       };
     };
